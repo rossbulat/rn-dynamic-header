ae0d6d32381d31ee810b23d8173fe21a7efa522f
Ross Bulat <ross@jkrbinvestments.com>
2020-01-02 10:02:54 +0700

    App const

--- App.tsx
+++ App.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import { ScrollContextProvider } from './ScrollContext'
 import { Document } from './Document'
 
-export default function App () {
-  return (
-    <ScrollContextProvider>
-      <Document />
-    </ScrollContextProvider>
-  );
-}
+export const App = () =>
+  <ScrollContextProvider>
+    <Document />
+  </ScrollContextProvider>;
+
+export default App;
